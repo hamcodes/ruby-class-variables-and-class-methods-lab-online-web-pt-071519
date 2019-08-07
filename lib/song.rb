cd3b85608578ec967828ca8fe229921afd971023
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-07 14:01:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,30 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   @@count = 0 
-  
+  @@genres = []
+  @@artists = []
   def initialize
     @@count += 1 
+    @@genres << []
+    @@artists += 1
   end
   
   def self.count
     return Song.count
   end
   
+  def self.genres 
+    @@genres = []
+  end
   
+  def self.artists
+  end
+
+  def self.genre_count
+  end
   
+  def self.artist_count
+  end
   
   
 end
