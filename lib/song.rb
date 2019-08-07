5e460adbcc6a01152042126ee49592671a1212ec
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-07 14:06:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song 
   attr_accessor :name, :artist, :genre 
-  # @@count = 0 
-  # @@genres = []
-  # @@artists = []
+  @@count = 0 
+  @@genres = []
+  @@artists = []
   
   def initialize
-    # @@count += 1 
+    @@count += 1 
     # @@genres << []
     # @@artists += 1
   end
